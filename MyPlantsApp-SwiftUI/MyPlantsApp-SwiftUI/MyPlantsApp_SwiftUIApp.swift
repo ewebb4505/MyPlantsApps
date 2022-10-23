3c26a0651ffe6551f3cce16d88cae24fd437a5ab
ewebb4505 <ewebb4505@gmail.com>
2022-10-23 13:19:03 -0400

    initial commit

---
+++ MyPlantsApp-SwiftUI/MyPlantsApp-SwiftUI/MyPlantsApp_SwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyPlantsApp_SwiftUIApp.swift
+//  MyPlantsApp-SwiftUI
+//
+//  Created by Evan Webb on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MyPlantsApp_SwiftUIApp: App {
+    //let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+                //.environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
